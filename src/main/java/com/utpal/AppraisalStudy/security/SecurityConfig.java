9ecd16cf0f1c427711841a179df357279c93ad14
utpal.kant <utpal.kant@beehyv.com>
2024-03-21 10:01:27 +0530

    password Hashing

---
+++ src/main/java/com/utpal/AppraisalStudy/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.utpal.AppraisalStudy.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeRequests().anyRequest().permitAll();
+        return httpSecurity.build();
+    }
+
+}
